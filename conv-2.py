e66b13eda89122bd53db36dd011b9da768d9a007
kikigogo9 <kiki.gogo99@gmail.com>
2023-05-01 23:27:53 +0200

    nonconvexity check v1

--- conv-2.py
+++ conv-2.py
@@ -1,40 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-# Generate random points with noise
-N = 20
-X = np.linspace(-1, 1, N)
-Y = np.sin(np.pi * X) + np.random.normal(scale=0.1, size=N)
-points = np.vstack((X, Y)).T
+# Generate random data
+n = 100
+x = np.linspace(0, 1, n)
+y = 2 * x + np.random.normal(size=n)
 
-# Define a function to check if a point is convex with respect to its closest neighbors
-def is_convex(points, index, k):
-    neighbors = points[np.argsort(np.linalg.norm(points - points[index], axis=1)), :]
-    n = len(neighbors)
-    p1 = neighbors[(index - 1) % n]
-    p2 = neighbors[index]
-    p3 = neighbors[(index + 1) % n]
-    cross_product = np.cross(p2 - p1, p3 - p2)
-    return cross_product >= 0
+# Reshape the data to fit the expected input shape for the LinearRegression model
+X = x.reshape((-1, 1))
+print(y)
+# Fit a linear regression model to the data
+model = LinearRegression()
+model.fit(X, y)
 
-# Identify the non-convex points
-nonconvex_inliers = []
-k = 2  # number of neighbors to consider
-for i in range(N):
-    if not is_convex(points, i, k):
-        nonconvex_inliers.append(i)
+# Get the slope and intercept of the line
+slope = model.coef_[0]
+intercept = model.intercept_
 
-# Plot the set of points
-plt.scatter(points[:, 0], points[:, 1])
-
-# Plot the non-convex points in blue
-nonconvex_inliers = np.array(nonconvex_inliers)
-plt.scatter(points[nonconvex_inliers, 0], points[nonconvex_inliers, 1], c='b', s=100)
-
-# Add labels and title
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Non-convex points with respect to closest neighbors')
-
-# Show the plot
-plt.show()
+# Print the results
+print(f"Slope: {slope:.2f}")
+print(f"Intercept: {intercept:.2f}")
--- convexity_check.py
+++ convexity_check.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib.pyplot import plot
 from matplotlib import pyplot as plt
 #sorted points with regards to its x value
-def convexity(points):
+def convexity(points, n=4, k=3):
     convex = []
     non_convex = []
     prev_slope = 0
@@ -17,8 +17,36 @@ def convexity(points):
             convex += [points[i]]
             non_convex += [points[i]]
         prev_slope = slope
+    # Check wether in the nearest n points are more convex than nonconvex points
+    for i in range(k):
+        convex, non_convex = renormalize(points, convex, non_convex, n=n)
     
     return convex, non_convex
+
+def renormalize(points, convex, non_convex, n=4):
+    res_c = []
+    res_nc = []
+
+    for i in range(len(points)):
+        convex_score = 0
+        non_convex_score = 0
+        for j in range(-n//2, n//2+1):
+            index = min(max(0, i+j), len(points)-1)
+            if points[index] in convex:
+                convex_score +=1
+            if points[index] in non_convex:
+                non_convex_score +=1
+            
+        if convex_score > non_convex_score:
+            res_c += [points[i]]
+        if convex_score < non_convex_score:
+            res_nc += [points[i]]
+        if convex_score == non_convex_score:
+            res_c += [points[i]]
+            res_nc += [points[i]]
+    
+    return res_c, res_nc
+
 
 points = []
 
@@ -26,10 +54,12 @@ def sin(x):
     r = np.random.random()
     return ((x+r)/10, np.sin((x+r)/10))
 
-def noisy_sin(x, noise_level = 0.4):
+def noisy_sin(x, noise_level = 0.1):
     r = np.random.random()
-    y_noise = np.random.random()*noise_level - noise_level/2
+    y_noise = np.random.normal(scale=noise_level)
     return ((x+r)/10, np.sin((x+r)/10 + y_noise))
+
+
 
 for i in range(100):
     points += [sin(i)]
@@ -41,6 +71,7 @@ x = np.linspace(0, 10, 100)
 
 # Calculate y values using sin function
 y = np.sin(x)
+
 
 print(c, nc)
 plot(list(map(lambda x: x[0], c)), list(map(lambda x: x[1], c)), 'bo')
